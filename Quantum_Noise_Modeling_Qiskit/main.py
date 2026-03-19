5e8efbdb7d3657d62c99dff14ef000c988a49d7d
Devarsh Panchal <devarshnotess@gmail.com>
2026-03-19 12:57:36 +0530

    Update and rename Qiskit to main.py

---
+++ Quantum_Noise_Modeling_Qiskit/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit_aer.noise import NoiseModel, depolarizing_error, amplitude_damping_error, phase_damping_error
+from qiskit.quantum_info import Statevector, state_fidelity
